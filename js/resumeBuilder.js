a6b63106fb9b2b89731a6576315af4181d7aaa6a
Jcook894 <Jcook894@icloud.com>
2016-07-01 21:40:54 -0400

    Finished up bio just have to fix img and add skills

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
+//Name and role
+//
+//////////////////////
+
+
 var formattedName = HTMLheaderName.replace("%data%","Julian Cook");
 
 $("#header").prepend(formattedName);
@@ -5,3 +10,40 @@ $("#header").prepend(formattedName);
 var formattedRole = HTMLheaderRole.replace("%data%","Front-End Web developer");
 
 $("#header").append(formattedRole);
+
+
+//bio area
+//
+////////////////
+
+var bio = {
+  "name" : "Julian Cook",
+  "role" : "Web Developer",
+  "contacts" : {
+    "mobile" : "860-614-9115",
+    "email" : "jcook894@icloud.com",
+    "github" : "https://github.com/Jcook894",
+    "location" : "Hartford,CT"
+
+  },
+  "welcomeMessage" : "Welcome to the Resume!",
+  "skills" : [
+    "HTML","CSS","JavaScript"
+  ],
+  "biopic" : "images/Me.jpg"
+}
+
+
+var formattedMobile = HTMLmobile.replace("%data%",bio.contacts.mobile);
+var formattedEmail = HTMLemail.replace("%data%",bio.contacts.email);
+var formattedGithub = HTMLgithub.replace("%data%",bio.contacts.github);
+var formattedLocation = HTMLlocation.replace("%data%",bio.contacts.location);
+var formattedWelcome = HTMLwelcomeMsg.replace("%data%",bio.welcomeMessage);
+var formattedBioPic = HTMLbioPic.replace("%data%","bio.biopic");
+
+$("#header").prepend(formattedBioPic);
+$("#header").append(formattedMobile);
+$("#header").append(formattedEmail);
+$("#header").append(formattedGithub);
+$("#header").append(formattedLocation);
+$("#header").append(formattedWelcome);
