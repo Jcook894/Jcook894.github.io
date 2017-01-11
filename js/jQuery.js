5d3d4312b6584b1d94613efe85addf1a15dde671
Jcook894 <Jcook894@icloud.com>
2017-01-11 01:00:33 -0500

    feat: still working on api fix on git pages.

--- js/jQuery.js
+++ js/jQuery.js
@@ -2191,7 +2191,7 @@ Expr = Sizzle.selectors = {
 
     "text": function( elem ) {
       var attr;
-      // IE6 and 7 will map elem.type to 'text' for new HTML5 types (search, etc)
+      // IE6 and 7 will `map` elem.type to 'text' for new HTML5 types (search, etc)
       // use getAttribute instead to test this case
       return elem.nodeName.toLowerCase() === "input" &&
         elem.type === "text" &&
